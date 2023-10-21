e4d6924b874ef2c9f105ff7d000990a92ece093c
Stephen Stephenson <stephenson.inbox@gmail.com>
2023-10-21 17:02:36 +0200

    Feat: Add Collection

---
+++ src/class/Collection.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Blocks\Tools;
+
+use Iterator;
+use Countable;
+
+abstract class Collection implements Iterator, Countable {
+    protected $position = 0;
+
+    protected array $array;
+
+    public function __construct(array $items) {
+        $this->array = [];
+
+        if ( !empty($items) ) {
+            if ( is_array($items) ) {
+                $this->array = $items;
+            } else {
+                $this->array[] = $items;
+            }
+        }
+    }
+
+    public function rewind(): void {
+        $this->position = 0;
+    }
+
+    abstract public function current();
+
+    #[\ReturnTypeWillChange]
+    public function key() {
+        return $this->position;
+    }
+
+    public function next(): void {
+        ++$this->position;
+    }
+
+    public function valid(): bool {
+        return isset($this->array[ $this->position ]);
+    }
+
+    public function count(): int {
+        return count($this->array);
+    }
+}
