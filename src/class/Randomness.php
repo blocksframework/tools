2c53425a257ab8de8f9d4335dbcdc26f73653b04
Stephen Stephenson <stephenson.inbox@gmail.com>
2023-10-22 19:57:30 +0200

    Style: Formatting

--- src/class/Randomness.php
+++ src/class/Randomness.php
@@ -3,45 +3,42 @@
 namespace Blocks\Tools;
 
 class Randomness {
+    public const NUMERIC = '0123456789';
+    public const ALPHANUMERIC = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    public const ALPHANUMERIC_WITHOUT_SIMILARLY_LOOKING_CHARACTERS = 'ABCDEFGHJKLMNPQRTUVWXY346789';
+    public const ALPHANUMERIC_SPECIAL_CHARACTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789~!@#$%^&*()_-+={}[]\|:;,./';
 
-    const NUMERIC = '0123456789';
-    const ALPHANUMERIC = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    const ALPHANUMERIC_WITHOUT_SIMILARLY_LOOKING_CHARACTERS = 'ABCDEFGHJKLMNPQRTUVWXY346789';
-    const ALPHANUMERIC_SPECIAL_CHARACTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789~!@#$%^&*()_-+={}[]\|:;,./';
-
-    static function generateString($length, $custom_chars = false) {
-        if ($custom_chars) {
+    public static function generateString( $length, $custom_chars = false ) {
+        if ( $custom_chars ) {
             $chars = $custom_chars;
-        } else {
+        }
+        else {
             $chars = Randomness::ALPHANUMERIC_SPECIAL_CHARACTERS;
         }
 
         $string = '';
 
-        $clen = mb_strlen($chars) - 1;
+        $clen = mb_strlen( $chars ) - 1;
 
-        for ($i = 0; $i < $length; $i++) {
-            $string .= $chars[ random_int(0, $clen) ];
+        for ( $i = 0; $i < $length; ++$i ) {
+            $string .= $chars[random_int( 0, $clen )];
         }
 
         return $string;
     }
 
-    static function generateToken($length) {
-        if ( $length%2 ) {
-            $adjusted_length = $length+1;
-        } else {
+    public static function generateToken( $length ) {
+        if ( $length % 2 ) {
+            $adjusted_length = $length + 1;
+        }
+        else {
             $adjusted_length = $length;
         }
 
-        $half_length = $adjusted_length/2;
+        $half_length = $adjusted_length / 2;
 
-        $token = bin2hex( random_bytes($half_length) );
+        $token = bin2hex( random_bytes( $half_length ) );
 
-        $token = mb_substr($token, 0, $length);
-
-        return $token;
+        return mb_substr( $token, 0, $length );
     }
-
 }
-
